8ec01b7715054d340921f5d9339d8d472e7badbf
Tomás Cano Santa Catalina <tomas.cano.santa.catalina@student.lut.fi>
2026-04-19 18:56:00 +0200

    GMM ML model addition

--- Code/ml_models.py
+++ Code/ml_models.py
@@ -1,6 +1,8 @@
+import numpy as np
 from sklearn.naive_bayes import MultinomialNB, GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
+from sklearn.mixture import GaussianMixture
 
 class MultinomialModel:
 
@@ -37,7 +39,7 @@ class GaussianModel:
 class LogisticModel:
 
     def __init__(self):
-        self.clf = LogisticRegression(max_iter=500)
+        self.clf = LogisticRegression(max_iter=200)
 
     def fit_predict(self, m_train, m_test, y_train):
         self.clf.fit(m_train, y_train)
@@ -60,3 +62,20 @@ class SVMModel:
     def predict(self, m_test):
         y_pred = self.clf.predict(m_test)
         return y_pred
+
+class GMMModel:
+    def __init__(self):
+        self.models = {}
+
+    def fit(self, X, y):
+        self.classes = np.unique(y)
+
+        for cls in self.classes:
+            X_c = X[y == cls]
+            gmm = GaussianMixture(n_components=1, covariance_type='full')
+            self.models[cls] = gmm.fit(X_c)
+
+    def predict(self, X):
+        likelihoods = np.array([mod.score_samples(X) for mod in self.models.values()]).T
+
+        return self.classes[np.argmax(likelihoods, axis=1)]
