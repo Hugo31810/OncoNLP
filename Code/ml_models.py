13c5c94902012eca859cd1ba382ec343a5ebb85d
Tomás Cano Santa Catalina <tomas.cano.santa.catalina@student.lut.fi>
2026-04-19 11:52:45 +0200

    Class ml models, folder update

--- Code/ml_models.py
+++ Code/ml_models.py
@@ -2,27 +2,61 @@ from sklearn.naive_bayes import MultinomialNB, GaussianNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC
 
-def multinomial_fit_predict(m_train, m_test, y_train):
-    # No compatible con embeddings
-    clf = MultinomialNB()
-    clf.fit(m_train,y_train)
-    y_pred = clf.predict(m_test)
-    return y_pred
+class MultinomialModel:
 
-def gaussian_fit_predict(m_train, m_test, y_train):
-    clf = GaussianNB()
-    clf.fit(m_train, y_train)
-    y_pred = clf.predict(m_test)
-    return y_pred
+    def __init__(self):
+        self.clf = MultinomialNB()
 
-def logistic_regression_fit_predict(m_train, m_test, y_train):
-    clf = LogisticRegression()
-    clf.fit(m_train, y_train)
-    y_pred = clf.predict(m_test)
-    return y_pred
+    def fit_predict(self, m_train, m_test, y_train):
+        # No compatible con embeddings
+        self.clf.fit(m_train,y_train)
+        y_pred = self.clf.predict(m_test)
+        return y_pred
+    
+    def predict(self, m_test):
+        y_pred = self.clf.predict(m_test)
+        return y_pred
 
-def svm_fit_predict(m_train, m_test, y_train):
-    clf = SVC(class_weight='balanced')
-    clf.fit(m_train, y_train)
-    y_pred = clf.predict(m_test)
-    return y_pred
+
+class GaussianModel:
+
+    def __init__(self):
+        self.clf = GaussianNB()
+
+
+    def fit_predict(self, m_train, m_test, y_train):
+        self.clf.fit(m_train, y_train)
+        y_pred = self.clf.predict(m_test)
+        return y_pred
+    
+    def predict(self, m_test):
+        y_pred = self.clf.predict(m_test)
+        return y_pred
+    
+
+class LogisticModel:
+
+    def __init__(self):
+        self.clf = LogisticRegression(max_iter=500)
+
+    def fit_predict(self, m_train, m_test, y_train):
+        self.clf.fit(m_train, y_train)
+        y_pred = self.clf.predict(m_test)
+        return y_pred
+    
+    def predict(self, m_test):
+        y_pred = self.clf.predict(m_test)
+        return y_pred
+    
+class SVMModel:
+    def __init__(self):
+        self.clf = SVC(class_weight='balanced')
+
+    def fit_predict(self, m_train, m_test, y_train):
+        self.clf.fit(m_train, y_train)
+        y_pred = self.clf.predict(m_test)
+        return y_pred
+    
+    def predict(self, m_test):
+        y_pred = self.clf.predict(m_test)
+        return y_pred
